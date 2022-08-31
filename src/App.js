454e278fcf83700750e3d3ce31d0c4213e040956
Shilpa S <shilpa.s@healthasyst.com>
2022-08-31 17:03:06 +0530

    End to end connectivity

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 
-import React, {useEffect} from 'react';
-import {init} from './Web3Client';
+import React, {useEffect, useState} from 'react';
+import {init, generateToken, getTokenUriByTokenId, ownerOf, getOwnerBalance} from './Web3Client';
 
 function App() {
 //const providerUrl = process.env.PROVIDER_URL || "http://localhost:8545";
+const [minted, setMinted] = useState(false);
+const [owner, setOwner] = useState("");
+const [balance, setBalance] = useState(0);
+
+const mintToken = ()=>{
+  generateToken().then(tx =>{
+    console.log(tx);
+    setMinted(true);
+  }).catch(err=>{
+    console.log(err)
+  })
+}
+
+const fetchFokenUriByTokenId = ()=>{
+  getTokenUriByTokenId(1).then(tx =>{
+    console.log(tx);
+    alert(tx);
+  }).catch(err=>{
+    console.log(err);
+  })
+}
+
+const getOwner = (tokenId)=>{
+  ownerOf(tokenId).then(tx=>{
+    console.log(tx);
+    setOwner(tx);
+  }).catch(err =>{
+    console.log(err);
+  })
+}
+
+const fetchOwnerBalance = ()=>{
+  getOwnerBalance().then(tx=>{
+    console.log(tx);
+    setBalance(tx);
+  }).catch(err =>{
+    console.log(err);
+  })
+}
+
 
 useEffect(() => {
   init();
 }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      {!minted ? <button onClick={()=> mintToken()}>Mint Token </button>
+      : <p>"Token minted successfully"</p>
+      }
+    <br />
+    <button onClick={() => fetchFokenUriByTokenId()}>Get Token URI</button>
+    <br />
+    <button onClick={()=> getOwner(8)}>Owner Of</button> <span>{owner}</span>
+    <br></br>
+    <button onClick={()=> fetchOwnerBalance()}>Refresh Balance</button>
+    <p>Your balance is {balance}</p>
+    </div>  
   );
 }
 
--- src/Web3Client.js
+++ src/Web3Client.js
@@ -1,25 +1,74 @@
 import Web3 from "web3";
+import BretflixNFTContract from 'contracts/BreitlexNFT.json'
 
 let selectedAccount;
+let nftContract;
+let isInitialized = false;
 
-export const init = () =>{
-    let provider = window.ethereum;
+function isMetaMaskInstalled() {
+  return Boolean(window.ethereum && window.ethereum.isMetaMask);
+}
 
-  if(typeof provider != undefined){
+export const init = async () =>{
+    let provider = window.ethereum;    
+
+  if(isMetaMaskInstalled()){
     //Metamask is installed
-
+    
     provider.request({method: 'eth_requestAccounts'}).then(accounts =>{
         selectedAccount = accounts[0];
-      console.log("Selected Account is ${selectedAccount}");
+      console.log('Selected Account is ${selectedAccount}');
     }).catch(err =>{
       console.log(err);
+      return; 
     });
 
     window.ethereum.on('accountsChanged', function (accounts){
         selectedAccount = accounts[0];
-        console.log("Selected Account changed to ${selectedAccount}");
+        console.log('Selected Account changed to ${selectedAccount}');
     })
+  }else{
+    alert("Metamask not available. Please install and retry.");
+    return;
   }
 
   const web3 = new Web3(provider);
+  const networkId = await web3.eth.net.getId();
+  nftContract = new web3.eth.Contract(BretflixNFTContract.abi, '0xe3Db97afc41721da4E9dF63ab2f9C9cf1DbE615a');
+
+  isInitialized = true;
+};
+
+export const generateToken = async () =>{
+  if(!isInitialized){
+    await init();
+  }
+  return nftContract.methods.mint(selectedAccount, selectedAccount).send({from: selectedAccount});
 }
+
+export const getTokenUriByTokenId = async (tokenid) =>{
+if(!isInitialized){
+  await init();
+}
+
+  return nftContract.methods.tokenURI(tokenid);
+}
+
+export const ownerOf = async (tokenid)=>{
+  if(!isInitialized){
+    await init();
+  }
+
+  return nftContract.methods.ownerOf(tokenid);
+}
+
+export const getOwnerBalance = async ()=>{
+  if(!isInitialized){
+    await init();
+  }
+
+  return nftContract.methods.balanceOf(selectedAccount).call().then(balance =>{
+    return Web3.utils.fromWei(balance);
+  });
+}
+
